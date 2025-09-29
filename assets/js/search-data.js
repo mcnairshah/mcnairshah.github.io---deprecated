a07de2beb06221c808e1dad121bb5205789d2efa
BucketofJava <bucketofjava@gmail.com>
2025-09-29 06:31:24 +0000

    Deploying to gh-pages from @ mcnairshah/mcnairshah.github.io@0ce711ee3e2780de50b1d067239a2645701bbaa9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,46 +414,26 @@ ninja.data = [{
           description: "Making Research More Accessible",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-eeg-decoding",
+          title: 'EEG Decoding',
+          description: "Understanding Brain Data with Machine Learning",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-the-equation-game",
+          title: 'The Equation Game',
+          description: "A daily equation game",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-thingnamer",
+          title: 'ThingNamer',
+          description: "An AI Interpretability-Based Object namer",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-page-by-page",
+          title: 'Page-By-Page',
+          description: "Making Research More Accessible",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/template/";
             },},{
         id: 'social-email',
         title: 'email',
