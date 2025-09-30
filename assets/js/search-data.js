fb5c999584b904e9d1a5b0e73556d86b899170d5
BucketofJava <bucketofjava@gmail.com>
2025-09-30 01:28:36 +0000

    Deploying to gh-pages from @ mcnairshah/mcnairshah.github.io@4344322bfdb1e11a8d8ff9d90ec848625d2bf3cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,11 +420,11 @@ ninja.data = [{
           description: "An AI Interpretability-Based Object namer",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-page-by-page",
-          title: 'Page-By-Page',
-          description: "Making Research More Accessible",
+            },},{id: "projects-thingnamer",
+          title: 'ThingNamer',
+          description: "An AI Interpretability-Based Object namer",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/a/template/";
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
